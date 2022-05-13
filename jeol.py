bd43b1b6d571e7f255905036af04fd7449b7ce69
Lolan <102511853+Jeolpaul@users.noreply.github.com>
2022-05-13 21:54:31 +0530

    Update jeol.py

--- jeol.py
+++ jeol.py
@@ -1,6 +1,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.types import CallbackQuery
+import asyncio
 
 
 Jeol=Client(
