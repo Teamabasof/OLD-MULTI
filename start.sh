43c5783a16beb06fac7d7e641dcdb90396452999
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-05-13 22:12:11 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,12 @@
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/Jeolpaul/pyrobotjeol/pulls.git /pyrobotjeol     
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /pyrobotjeol
+fi
+cd /pyrobotjeol
+pip3 install -U -r requirements.txt
+echo "BOT IS STARTING⚡️⚡️⚡️"
+python3 jeol.py
