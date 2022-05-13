46b662755f79263e51c97321635f78a5366aa665
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-05-14 00:45:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,42 +15,19 @@ API_HASH = os.environ.get("API_HASH", "")
 
 FORCE_SUB = os.environ.get("FORCE_SUB", None)           
 
-class Client(Client):
 
-    def __init__(self):
-        super().__init__(
-            name="renamer",
-            api_id=API_ID,
-            api_hash=API_HASH,
-            bot_token=BOT_TOKEN,
-            workers=50,
-            plugins={"root": "plugins"},
-            sleep_threshold=5,
-        )
-
-    async def start(self):
-       await super().start()
-       me = await self.get_me() 
-       self.mention = me.mention
-       self.username = me.username 
-       self.force_channel = FORCE_SUB
-       if FORCE_SUB:
-         try:
-            link = await self.export_chat_invite_link(FORCE_SUB)
-            self.invitelink = link
-         except Exception as e:
-            logging.warning(e) 
-            logging.warning("Make Sure Bot admin in force sub channel") 
-            self.force_channel = None
-       logging.info(f"{me.first_name} Started")
-        
-    async def stop(self, *args):
-      await super().stop()
-      logging.info("Bot Stopped")
-        
-bot = Client()
-bot.run()
-
+if __name__ == "__main__" :
+    plugins = dict(
+        root="plugins"
+    )
+    app = Client(
+        "renamer",
+        bot_token=BOT_TOKEN,
+        api_id=API_ID,
+        api_hash=API_HASH,
+        plugins=plugins
+    )
+    app.run()
 
 
 
