a97304b262451b818ddafd6833df30c2fcb66e4c
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-05-14 14:05:43 +0530

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,6 +1,12 @@
-import os
+import re
+from os import environ
 
+id_pattern = re.compile(r'^.\d+$')
 
 STAT_STICK = os.environ.get("STAT_STICK", "CAACAgUAAxkBAAIMMmJ_Y17NRUpBJgLhsqUTTRNilYxAAAKeBAACf7TwVxZUQiDRe7p1HgQ")
 
 PICS = os.environ.get("PICS", "https://telegra.ph/file/34fd203eb89fd747ffb57.jpg").split()
+
+ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMIN', '').split()]
+
+DB_URL = os.environ.get("DB_URL","")
