8f4738bb562fd0a8883d1ce4c757805f1f6f291d
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-05-14 08:44:32 +0530

    Update Callback.py

--- plugins/Callback.py
+++ plugins/Callback.py
@@ -1,1 +1,42 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton                              
+from pyrogram.types import CallbackQuery
+from config import FORCE_SUB
+import asyncio
+
+
+@Client.on_callback_query()
+async def callback(bot, msg):
+   if msg.data == "commands":
+       await msg.message.edit(
+           text="""HEY HERE YOU CAN FIND THE BASIC COMMANDS OF MINE.IF YOU DON'T KNOW HOW TO USE COMMAND JOIN SUPPORT GROUP AND ASK.
+COMMANDS - /start /help /id /info /bots THESE WERE OUR COMMANS WE HAVE PUBLISHED"""
+       )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
