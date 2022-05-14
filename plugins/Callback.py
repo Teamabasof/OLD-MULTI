74647d91e38fe6386544b4092fd3b03f4c6bcbde
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-05-14 09:06:08 +0530

    Update Callback.py

--- plugins/Callback.py
+++ plugins/Callback.py
@@ -6,12 +6,26 @@ import asyncio
 
 @Client.on_callback_query()
 async def callback(bot, msg):
-   if msg.data == "commands":
+   data = msg.data
+   if data == "commands":
        await msg.message.edit(
            text="""HEY HERE YOU CAN FIND THE BASIC COMMANDS OF MINE.IF YOU DON'T KNOW HOW TO USE COMMAND JOIN SUPPORT GROUP AND ASK.
 COMMANDS - /start /help /id /info /bots THESE WERE OUR COMMANS WE HAVE PUBLISHED"""
        )
-
+   elif data == "about"
+         await msg.message.edit(
+             text=""""
+╔════❰ 𝙼𝚄𝙻𝚃𝙸 𝙱𝙾𝚃 ❱═❍
+║╭━━━━━━━━━━━━━━━➣
+║┣⪼👦ᴄʀᴇᴀᴛᴏʀ : ᴊᴇᴏʟᴘᴀᴜʟ
+║┣⪼📡ʜᴏsᴛᴇᴅ ᴏɴ : ʜᴇʀᴏᴋᴜ
+║┣⪼🗣️ʟᴀɴɢᴜᴀɢᴇ : ᴘʏᴛʜᴏɴ3
+║┣⪼📚ʟɪʙʀᴀʀʏ : ᴘʏʀᴏɢʀᴀᴍ 
+║┣⪼🗒️ᴠᴇʀsɪᴏɴ : 1.0.3  
+║╰━━━━━━━━━━━━━━━➣
+╚══════════════════❍  
+"""
+         )     
 
 
 
