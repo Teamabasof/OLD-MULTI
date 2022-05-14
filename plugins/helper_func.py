5c2bc8d2c123e5e9fcf344fc137ca829b0f3189b
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-05-14 08:44:28 +0530

    Update helper_func.py

--- plugins/helper_func.py
+++ plugins/helper_func.py
@@ -114,10 +114,4 @@ async def bots_message(bot, message):
 
 
 
-@Client.on_callback_query()
-async def callback(bot, msg):
-   if msg.data == "commands":
-       await msg.message.edit(
-           text="""HEY HERE YOU CAN FIND THE BASIC COMMANDS OF MINE.IF YOU DON'T KNOW HOW TO USE COMMAND JOIN SUPPORT GROUP AND ASK.
-COMMANDS - /start /help /id /info /bots THESE WERE OUR COMMANS WE HAVE PUBLISHED"""
-       )
+
