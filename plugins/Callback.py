16acf10f17bb7656cfcae23ffe34e6b27b29f617
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-05-14 08:47:00 +0530

    Update Callback.py

--- plugins/Callback.py
+++ plugins/Callback.py
@@ -1,7 +1,6 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton                              
 from pyrogram.types import CallbackQuery
-from config import FORCE_SUB
 import asyncio
 
 
