cd68d0b8f6969c6d2f7097dd092dab9298c6f4f4
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-05-14 13:42:15 +0530

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,6 @@
+import os
+
+
+STAT_STICK = os.environ.get("STAT_STICK", "CAACAgUAAxkBAAIMMmJ_Y17NRUpBJgLhsqUTTRNilYxAAAKeBAACf7TwVxZUQiDRe7p1HgQ")
+
+PICS = os.environ.get("PICS", "https://telegra.ph/file/34fd203eb89fd747ffb57.jpg").split()
