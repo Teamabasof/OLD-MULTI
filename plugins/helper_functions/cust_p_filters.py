a5000aede270e755559f279ee3070e302bf63a5e
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-05-14 14:50:25 +0530

    Create cust_p_filters.py

---
+++ plugins/helper_functions/cust_p_filters.py
@@ -0,0 +1,24 @@
+from pyrogram import (
+    filters
+)
+import os
+
+USE_AS_BOT = os.environ.get("USE_AS_BOT", True)
+
+
+def onw_filter(filt, client, message):
+    if USE_AS_BOT:
+        return bool(
+            True # message.from_user.id in ADMINS
+        )
+    else:
+        return bool(
+            message.from_user and
+            message.from_user.is_self
+        )
+
+
+f_onw_fliter = filters.create(
+    func=onw_filter,
+    name="OnwFilter"
+)
