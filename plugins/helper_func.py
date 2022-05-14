dcdb193c90e8d8b406f9e924db04f240d5d4c7e6
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-05-14 09:38:35 +0530

    Update helper_func.py

--- plugins/helper_func.py
+++ plugins/helper_func.py
@@ -112,6 +112,12 @@ async def bots_message(bot, message):
             )
         )
 
+@Client.on_message(filters.command(["stickerid"]))
+async def stickerid(bot, message):   
+    if message.reply_to_message.sticker:
+       await message.reply(f"**Sticker ID is**  \n `{message.reply_to_message.sticker.file_id}` \n \n ** Unique ID is ** \n\n`{message.reply_to_message.sticker.file_unique_id}`", quote=True)
+    else: 
+       await message.reply("Oops !! Not a sticker file")
 
 
 
