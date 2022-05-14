3126ccd84bb7f85ca59f0554259a02c347b2d14f
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-05-14 19:30:38 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Jeolpaul/pyrobotjeol/pulls.git /pyrobotjeol     
+  git clone https://github.com/Jeolpaul/TG-MULTI-BOT.git /TG-MULTI-BOT     
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /pyrobotjeol
+  git clone $UPSTREAM_REPO /TG-MULTI-BOT
 fi
-cd /pyrobotjeol
+cd /TG-MULTI-BOT
 pip3 install -U -r requirements.txt
 echo "BOT IS STARTING⚡️⚡️⚡️"
 python3 config.py
