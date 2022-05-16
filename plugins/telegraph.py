c88af8dd45a0e7dd30a8d27558906e60e3e1730a
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-05-16 21:23:02 +0530

    Update telegraph.py

--- plugins/telegraph.py
+++ plugins/telegraph.py
@@ -4,6 +4,15 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message, CallbackQuery
 from telegraph import upload_file
 
+@Client.on_message(filters.private & filters.create(not_subscribed))
+async def is_not_subscribed(client, message):
+    await message.reply_text(
+       text="**⚠️Sorry bro,You didn't Joined Our Updates Channel Join now and start again🙏**",
+       reply_markup=InlineKeyboardMarkup([
+           [ InlineKeyboardButton(text="📢𝙹𝚘𝚒𝚗 𝙼𝚢 𝚄𝚙𝚍𝚊𝚝𝚎 𝙲𝚑𝚊𝚗𝚗𝚎𝚕📢", url=client.invitelink)]
+           ])
+       )
+
 @Client.on_message(filters.private & filters.command(["tgmedia", "tgraph", "telegraph"]))
 async def telegraph(client, message):
     replied = message.reply_to_message
