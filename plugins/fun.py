187eddbed3c7ff4b9b57e33dfcfec6f00fe1f5cf
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-05-16 21:20:09 +0530

    Update fun.py

--- plugins/fun.py
+++ plugins/fun.py
@@ -54,32 +54,32 @@ IKKA_STRINGS = (
     "CAACAgUAAxkBAAIDkmIN87LWn-56jo9wHTdifHsdBCAiAAJPAwACK4yZVlCyU1tXbk2YHgQ",
 )   
 
-@Client.on_message(filters.private & filters.command("dice"))
+@Client.on_message(filters.command("dice"))
 async def roll_dice(bot, message):
     await bot.send_dice(message.chat.id, "🎲")
 
-@Client.on_message(filters.private & filters.command("arrow"))                                      
+@Client.on_message(filters.command("arrow"))                                      
 async def roll_arrow(bot, message):
     await bot.send_dice(message.chat.id, "🎯")
 
-@Client.on_message(filters.private & filters.command("goal"))
+@Client.on_message(filters.command("goal"))
 async def roll_goal(bot, message):
     await bot.send_dice(message.chat.id, "⚽️")
 
-@Client.on_message(filters.private & filters.command("luck"))
+@Client.on_message(filters.command("luck"))
 async def roll_luck(bot, message):
     await bot.send_dice(message.chat.id, "🎰")
 
-@Client.on_message(filters.private & filters.command("throw"))
+@Client.on_message(filters.command("throw"))
 async def roll_throw(bot, message):
     await bot.send_dice(message.chat.id, "🏀")
 
-@Client.on_message(filters.private & filters.command(["bowling", "tenpins"]))
+@Client.on_message(filters.command(["bowling", "tenpins"]))
 async def roll_bowling(bot, message):
     await bot.send_dice(message.chat.id, "🎳")
 
 
-@Client.on_message(filters.private & filters.command("runs") &
+@Client.on_message(filters.command("runs") &
     f_onw_fliter
 )
 async def runs(bot, message):
@@ -90,7 +90,7 @@ async def runs(bot, message):
     else:
         await message.reply_text(effective_string)
 
-@Client.on_message(filters.private & filters.command("ikka") &
+@Client.on_message(filters.command("ikka") &
     f_onw_fliter
 )
 async def ikka(bot, message):
