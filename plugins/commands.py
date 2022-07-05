14bb6e97212e6b05a34ee92ec265aefe8e0baefc
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-07-05 22:08:22 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -13,7 +13,7 @@ import random
 #@Client.on_message(filters.private & filters.create(not_subscribed))
 async def is_not_subscribed(client, message):
     await message.reply_text(
-       text="**⚠️Sorry bro,You didn't Joined Our Updates Channel Join now and start again🙏**",
+       text="**🗣️Sorry bro,You didn't Joined Our Updates Channel Join now and start again🙏**",
        reply_markup=InlineKeyboardMarkup( [[
            InlineKeyboardButton(text="📢𝙹𝚘𝚒𝚗 𝙼𝚢 𝚄𝚙𝚍𝚊𝚝𝚎 𝙲𝚑𝚊𝚗𝚗𝚎𝚕📢", url=client.invitelink)
            ],[
