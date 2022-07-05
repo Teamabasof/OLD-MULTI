1c37d09e2e03cf46d198eb1d41df2ddd788a4004
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-07-05 18:12:10 +0530

    Update logo_maker.py

--- plugins/logo_maker.py
+++ plugins/logo_maker.py
@@ -3,6 +3,8 @@ from typing import Optional
 import aiohttp
 from config import LOGO_API_URL1, LOGO_API_URL2
 
+session = aiohttp.ClientSession()
+
 async def generate_logo(text: str, square: Optional[bool] = False ):
   "To Create Logos. text = What you want to write on the logo. square = If You Want Square Logo Or Not. Returns Telgraph Image Url"
   
