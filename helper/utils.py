5cdef4f6db8ce08df4b09e26670116394a4c4fe9
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-07-05 20:57:17 +0530

    Update utils.py

--- helper/utils.py
+++ helper/utils.py
@@ -18,8 +18,7 @@ async def ForceSub(bot: Client, cmd: Message):
                 disable_web_page_preview=True
             )
             return 400
-    except UserNotParticipant:
-        invite_link = await bot.create_chat_invite_link(chat_id=(int(FORCE_SUB) if FORCE_SUB.startswith("-100") else FORCE_SUB))
+    except UserNotParticipant:        
         await bot.send_message(                               
             chat_id=cmd.from_user.id,
             text="**Please Join My Updates Channel to use this Bot!**\n\nDue to Overload, Only Channel Subscribers can use the Bot!",
