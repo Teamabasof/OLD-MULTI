05cefc9c14ca645822fb64e9b15fd6adcff82790
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-07-05 20:15:56 +0530

    Update utils.py

--- helper/utils.py
+++ helper/utils.py
@@ -20,11 +20,10 @@ async def ForceSub(bot: Client, cmd: Message):
         await bot.send_message(            
             #chat_id=cmd.from_user.id,
             text="**Please Join My Updates Channel to use this Bot!**\n\nDue to Overload, Only Channel Subscribers can use the Bot!",
-            invite=f'https://t.me/{FORCE_SUB}',
             reply_markup=InlineKeyboardMarkup( [[
-                InlineKeyboardButton(text="📢 Join Updates Channel 📢", url=invite)
+                InlineKeyboardButton(text='📢 Join Updates Channel 📢', url=f'https://t.me/{FORCE_SUB}',)
                 ],[
-                InlineKeyboardButton("🔄 𝚃𝚛𝚢 𝙰𝚐𝚊𝚒𝚗 🔄", url=f"https://t.me/{bot.username}?start=start")                            
+                InlineKeyboardButton('🔄 𝚃𝚛𝚢 𝙰𝚐𝚊𝚒𝚗 🔄', url=f'https://t.me/{bot.username}?start=start')                            
                 ]]
             ),
             parse_mode="markdown"
