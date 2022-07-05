f4dd3f49f469ebe7d8248ffc74d3c6bc3de17bec
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-07-05 19:57:11 +0530

    Update loader.py

--- loader.py
+++ loader.py
@@ -1,13 +1,7 @@
 import os
 import logging 
-import logging.config
 from pyrogram import Client
 import re
-
-                            
-logging.config.fileConfig('logging.conf')
-logging.getLogger().setLevel(logging.INFO)
-logging.getLogger("pyrogram").setLevel(logging.ERROR)             
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
@@ -31,11 +25,7 @@ class App(Client):
        me = await self.get_me() 
        self.mention = me.mention
        self.username = me.username        
-       logging.info(f"{me.first_name} Started")
-        
-    async def stop(self, *args):
-      await super().stop()
-      logging.info("Bot Stopped")
+       
 
 bot = App()
 bot.run()
