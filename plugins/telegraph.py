db573bf85ad9e0b34678312fe492b48da4e713de
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-07-05 21:32:35 +0530

    Update telegraph.py

--- plugins/telegraph.py
+++ plugins/telegraph.py
@@ -2,13 +2,22 @@ import os
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message, CallbackQuery
 from telegraph import upload_file
-from helper.utils import ForceSub
+from helper.utils import not_subscribed
+
+@Client.on_message(filters.private & filters.create(not_subscribed))
+async def is_not_subscribed(client, message):
+    await message.reply_text(
+       text="**⚠️Sorry bro,You didn't Joined Our Updates Channel Join now and start again🙏**",
+       reply_markup=InlineKeyboardMarkup( [[
+           InlineKeyboardButton(text="📢𝙹𝚘𝚒𝚗 𝙼𝚢 𝚄𝚙𝚍𝚊𝚝𝚎 𝙲𝚑𝚊𝚗𝚗𝚎𝚕📢", url=client.invitelink)
+           ],[
+           InlineKeyboardButton("🔄 𝚃𝚛𝚢 𝙰𝚐𝚊𝚒𝚗 🔄", url=f"https://t.me/{client.username}?start=start")            
+           ]]
+           )
+       )
 
 @Client.on_message(filters.private & filters.command(["tgmedia", "tgraph", "telegraph"]))
 async def telegraph(client, message):
-    forcesub = await ForceSub(client, message)
-    if forcesub == 400:
-        return
     replied = message.reply_to_message
     if not replied:
         await message.reply("Reply to a supported media file")
