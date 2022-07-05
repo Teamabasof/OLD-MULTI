980a44e9ad25bc51da6974d5f40d6685eb6f4a54
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-07-05 20:55:51 +0530

    Update loader.py

--- loader.py
+++ loader.py
@@ -2,6 +2,7 @@ import os
 import logging 
 from pyrogram import Client
 import re
+from variables import FORCE_SUB
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
@@ -25,7 +26,17 @@ class App(Client):
        me = await self.get_me() 
        self.mention = me.mention
        self.username = me.username        
-       
+       self.force_channel = FORCE_SUB
+       if FORCE_SUB:
+         try:
+            link = await self.export_chat_invite_link(FORCE_SUB)
+            self.invitelink = link
+         except Exception as e:
+            logging.warning(e) 
+            logging.warning("Make Sure Bot admin in force sub channel") 
+            self.force_channel = None
+
+
 print("Bot Started...")
 bot = App()
 bot.run()
