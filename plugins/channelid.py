1266855984d56c8495d229bd2bdf39a7c96d9412
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-07-06 17:37:50 +0530

    Update channelid.py

--- plugins/channelid.py
+++ plugins/channelid.py
@@ -1,10 +1,10 @@
 from pyrogram import filters
 from pyrogram import Client
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from helper.utils import not_subscribed
+#from helper.utils import not_subscribed
 from helper.ban import BanChek
 
-@Client.on_message(filters.private & filters.create(not_subscribed))
+#@Client.on_message(filters.private & filters.create(not_subscribed))
 async def is_not_subscribed(client, message):
     await message.reply_text(
        text="**⚠️Sorry bro,You didn't Joined Our Updates Channel Join now and start again🙏**",
