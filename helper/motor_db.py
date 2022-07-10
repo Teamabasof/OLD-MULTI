df8183ca3cf582ea74306aad907185187367d492
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-07-10 18:44:39 +0530

    Update and rename _$database.py___ to motor_db.py

--- helper/motor_db.py
+++ helper/motor_db.py
@@ -2,33 +2,29 @@ import motor.motor_asyncio, datetime
 from variables import DB_URL
 import os
 
-mongo = pymongo.MongoClient(DB_URL)
-
-
 class Database:
     
     def __init__(self, uri, database_name):
         self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
         self.db = self._client[database_name]
-        self.dcol = self.db.users
-        self.grp = self.db.groups
-        
+        self.col = self.db.users
+
     def new_user(self, id):
         return dict(
-            id = id,
-            join_date = datetime.date.today().isoformat()
-        )        
+            id=id,
+            join_date=datetime.date.today().isoformat(),            
+        )
 
     async def add_user(self, id):
         user = self.new_user(id)
-        await self.dcol.insert_one(user)
+        await self.col.insert_one(user)
 
     async def is_user_exist(self, id):
-        user = await self.dcol.find_one({'id':int(id)})
-        return bool(user)
+        user = await self.col.find_one({'id': int(id)})
+        return True if user else False
 
  
-db = Database(DB_URL, "Cloud19")
+db = Database(DB_URL, "logs")
 
 
 
