6e2e5bdf59169d1c4da13761cca9701162a4fa71
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-08-08 20:09:54 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -105,25 +105,25 @@ async def photoid(bot, message):
 
 @Client.on_message(filters.private & filters.user(ADMIN) & filters.command(["broadcast"]))
 async def broadcast(bot, message):
-    if (message.reply_to_message):
-      ms = await message.reply_text("Geting All ids from database ...........")
-      ids = getid()
-      tot = len(ids)
-      success = 0 
-      failed = 0
-      await ms.edit(f"Starting Broadcast .... \n Sending Message To {tot} Users")
-      for id in ids:
-        try:
-     	   time.sleep(1)
-     	   await message.reply_to_message.copy(id)
-           success += 1 
-        except:
-     	   failed += 1
-     	   pass
-        try:
-     	   await ms.edit( f"Message sent to {success} chat(s). {failed} chat(s) failed on receiving message. \nTotal - {tot}" )
-        except FloodWait as e:
-     	   await asyncio.sleep(t.x)
+ if (message.reply_to_message):
+   ms = await message.reply_text("Geting All ids from database ...........")
+   ids = getid()
+   tot = len(ids)
+   success = 0 
+   failed = 0 
+   await ms.edit(f"Starting Broadcast .... \n Sending Message To {tot} Users")
+   for id in ids:
+     try:
+     	time.sleep(0.5)
+     	await message.reply_to_message.copy(id)
+     	success += 1 
+     except:
+     	failed += 1 
+     	pass
+     try:
+     	await ms.edit( f"Message sent to {success} chat(s). {failed} chat(s) failed on receiving message. \nTotal - {tot}" )
+     except FloodWait as e:
+     	await asyncio.sleep(t.x)
 
 
 @Client.on_message(filters.private & filters.user(ADMIN) & filters.command(["users"]))
